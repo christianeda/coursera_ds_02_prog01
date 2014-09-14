a9fbbb0c626745150acfb5e7b0e99a3f850024fe
christianeda <christianeda@gmail.com>
2014-09-14 12:45:19 +0200

    setup

---
+++ code/raw/part1.R
@@ -0,0 +1,24 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+
+        ## 'pollutant' is a character vector of length 1 indicating
+        ## the name of the pollutant for which we will calculate the
+        ## mean; either "sulfate" or "nitrate".
+
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+
+        ## Return the mean of the pollutant across all monitors list
+        ## in the 'id' vector (ignoring NA values)
+}
+
+#sample_ouput
+
+source("pollutantmean.R")
+pollutantmean("specdata", "sulfate", 1:10)
+## [1] 4.064
+pollutantmean("specdata", "nitrate", 70:72)
+## [1] 1.706
+pollutantmean("specdata", "nitrate", 23)
+## [1] 1.281
